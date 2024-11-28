5a3eff5d096b0d9402d63ce3497f1c1dacb8b9b0
RdAnjos <rodrigo.oda@hotmail.com>
2024-11-28 20:25:41 -0300

    Configurando DataContext e Crud

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Blog.Data;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog
 {
@@ -6,7 +11,41 @@ namespace Blog
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var context = new BlogDataContext())
+            {
+                // CREATE
+                // var tag = new Tag { Name = ".NET", Slug = "dotnet" };
+                // context.Tags.Add(tag);
+                // context.SaveChanges();
+
+                // var tag1 = new Tag { Name = "Angular", Slug = "angular" };
+                // context.Tags.Add(tag1);
+                // context.SaveChanges();
+
+                // //UPDATE
+                // var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
+                // tag.Name = "ASP-NET Core";
+                // tag.Slug = "asp-netcore";
+                // context.Update(tag);
+                // context.SaveChanges();
+
+                // // DELETE
+                // var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
+                // context.Remove(tag);
+                // context.SaveChanges();
+
+                var tags = context
+                        .Tags
+                        .AsNoTracking() // faz com que nao traga os METADADOS, traz ganho de performance nao recomendado para Delete ou Update
+                        .Where(x => x.Name.Contains(".net"))
+                        .ToList(); //boa pratica deixar list para final pois n pesquisara todo conteudo da base.
+
+                foreach (var tag in tags)
+                {
+                    System.Console.WriteLine($"{tag.Name}");
+                }
+
+            }
         }
     }
 }
