85936fef99b53db5632a54835186830e960fda74
RdAnjos <rodrigo.oda@hotmail.com>
2024-12-10 13:22:11 -0300

    Created User, Post and Category Map and tested

--- Data/BlogDataContext.cs
+++ Data/BlogDataContext.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Runtime.InteropServices;
+using Blog.Data.Mappings;
 using Blog.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,10 +8,20 @@ namespace Blog.Data
 {
     public class BlogDataContext : DbContext
     {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
-            options.UseSqlServer("Server=localhost,1433; Database=Blog-efcore;TrustServerCertificate=true;User ID=sa; Password= @Password24#");
+            options.UseSqlServer("Server=localhost,1433; Database=BlogFluentApi;TrustServerCertificate=true;User ID=sa; Password= @Password24#");
             //options.LogTo(Console.WriteLine);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new PostMap());
+            modelBuilder.ApplyConfiguration(new UserMap());
         }
     }
 }
