5209785cc8d4ecac3d583be0d54c540bd5cf0695
RdAnjos <rodrigo.oda@hotmail.com>
2024-12-11 19:34:52 -0300

    created the first database migrations

--- Program.cs
+++ Program.cs
@@ -13,41 +13,6 @@ namespace Blog
         static void Main(string[] args)
         {
             Console.Clear();
-
-            var context = new BlogDataContext();
-
-            // context.Users.Add(new User
-            // {
-            //     Name = "Rodrigo Oliveira",
-            //     Email = "rodrigo147@gmail.com",
-            //     Bio = "Software developer",
-            //     Image = "image.com/rodrigo",
-            //     PasswordHash = "pw1234",
-            //     Slug = "roa"
-            // });
-            var user = context.Users
-                        .FirstOrDefault(x => x.Id == 4);
-            var category = new Category
-            {
-                Name = "APIs",
-                Slug = "apis"
-            };
-
-            var post = new Post
-            {
-                Author = user,
-                Category = category,
-                Title = "Fluent API",
-                Summary = "Learning about how to do Fluent API",
-                Body = "<b>fluent-api</b>",
-                Slug = "fluent-api",
-                CreateDate = DateTime.Now.ToUniversalTime()
-                //LastUpdateDate = DateTime.Now.ToUniversalTime(),
-                //Tags = null
-            };
-            context.Posts.Add(post);
-            context.SaveChanges();
-
         }
     }
 }
