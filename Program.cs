2029e97ab85fd25c0004f535f62be28f441d396e
RdAnjos <rodrigo.oda@hotmail.com>
2024-12-12 10:37:33 -0300

    Created a new migrations version, added a new field in User Class

--- Program.cs
+++ Program.cs
@@ -13,6 +13,23 @@ namespace Blog
         static void Main(string[] args)
         {
             Console.Clear();
+
+            var context = new BlogDataContext();
+
+            var user = new User
+            {
+                Bio = "abc",
+                Email = "teste1@email.net",
+                Github = "teste@github.com",
+                Image = "https://",
+                Name = "Teste",
+                PasswordHash = "teste@123",
+                Slug = "teste-1"
+            };
+
+            context.Users.Add(user);
+            context.SaveChanges();
+
         }
     }
 }
